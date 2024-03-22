3e82a149bcb9d4184eb5200cc832a8dd34070c74
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-03-22 14:14:27 +0100

    fix(user-mode.js): genUsrMode, change id en #logout querySelec écoute id #logout pour se deconnecter

--- Frontend/user-mode.js
+++ Frontend/user-mode.js
@@ -1,20 +1,23 @@
+function isConnected() {
+    let status = window.sessionStorage.getItem("token");
+    status && genUserMode();
+}
+
 function genUserMode() {
     document.querySelector("#login").innerHTML = "logout";
+    document.querySelector("#login").id = "logout";
     let boutonModifier = document.querySelector(".bouton-modifier");
     boutonModifier.style.display = "flex";
     let modeEdition = document.querySelector(".mode-edition");
     modeEdition.style.display = "flex";
     document.querySelector(".filter-bar").style.visibility = "hidden";
 }
-function isConnected() {
-    let status = window.sessionStorage.getItem("token");
-    status && genUserMode();
-}
 
 //Process
 isConnected()
-document.querySelector("#login").addEventListener("click", function (event) {
-    window.sessionStorage.removeItem("token");
-    location.reload("index.html");
-
-})
+if (document.querySelector("#logout")) {
+    document.querySelector("#logout").addEventListener("click", function (event) {
+        window.sessionStorage.removeItem("token");
+        location.reload("index.html");
+    });
+}
